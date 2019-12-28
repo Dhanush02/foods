84115c3f0a6a4999699f6fa6eb499b1142b586ef
Dhanush02 <52448311+Dhanush02@users.noreply.github.com>
2019-12-28 14:49:08 +0530

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,36 @@
+    var flag=1;
+function abc(){
+    if(flag==1){
+        document.getElementById("abc").style.display="block";
+        flag=0;
+    }
+    else{
+        document.getElementById("abc").style.display="none";
+        flag=1;
+        
+    }
+    
+}
+function abc1(){
+    if(flag==1){
+        document.getElementById("abc1").style.display="block";
+        flag=0;
+    }
+    else{
+        document.getElementById("abc1").style.display="none";
+        flag=1;
+    }
+   
+    }
+
+function abc2(){
+if(flag==1){
+    document.getElementById("abc2").style.display="block";
+    flag=0;
+}
+else{
+    document.getElementById("abc2").style.display="none";
+    flag=1;
+}
+
+}
